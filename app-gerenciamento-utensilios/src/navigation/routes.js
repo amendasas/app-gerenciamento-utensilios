7cdab767305a940ba142fa06dde22dfa2863c256
Siade Lucas <siadereak@gmail.com>
2025-01-23 19:59:08 -0300

    Funciona pelo amor de Deus

---
+++ app-gerenciamento-utensilios/src/navigation/routes.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack'; // Import do Stack Navigator
+import TelaInicial from '../screens/telaInicial';
+import CadastroUtensilio from '../screens/cadastroUtensilio';
+
+const Stack = createStackNavigator();
+
+export default function Routes() {
+  return (
+    <Stack.Navigator initialRouteName="TelaInicial">
+        <Stack.Screen 
+            name="TelaInicial"
+            component={TelaInicial} 
+            options={{ headerShown: false }}
+        />
+
+        <Stack.Screen 
+            name="CadastroUtensilio"
+            component={CadastroUtensilio}
+            options={{ headerShown: false }}
+        />
+    </Stack.Navigator>
+  );
+}
